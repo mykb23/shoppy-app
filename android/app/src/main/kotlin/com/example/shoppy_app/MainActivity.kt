36dd4d2a59cf932e15445cfbb5d79b9ec555b74f
Michael Akinsuroju <makinsuroju@gmail.com>
2021-08-12 09:03:41 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/shoppy_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shoppy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color.fromRGBO(255, 110, 78, 1);
+Color dark = Color.fromRGBO(1, 0, 53, 1);
